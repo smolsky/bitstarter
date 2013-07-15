3ef8dd0130e184f9bdfc32928614079b14ca1e0a
Ubuntu <ubuntu@ip-172-31-27-244.us-west-2.compute.internal>
2013-07-15 05:44:38 +0000

    web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ ar express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = fs.readFileSync("index.html");
+//var buf = fs.readFileSync("index.html");
 
 //var str = buf.toString(); 
 
